bfb6a1184ecdf8859441a27dd48616adac4cb691
Dave <177987681+davidgyorffy@users.noreply.github.com>
2026-09-01 17:28:41 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "wallclock-v68";
-const APP_FILES = ["./index.html?v=68", "./manifest.json?v=68"];
+const CACHE_NAME = "wallclock-v69";
+const APP_FILES = ["./index.html?v=69", "./manifest.json?v=69"];
 
 self.addEventListener("install", function(event) {
   self.skipWaiting();
@@ -49,13 +49,13 @@ self.addEventListener("fetch", function(event) {
           if (response && response.ok) {
             var copy = response.clone();
             caches.open(CACHE_NAME).then(function(cache) {
-              cache.put("./index.html?v=68", copy).catch(function(){});
+              cache.put("./index.html?v=69", copy).catch(function(){});
             });
           }
           return response;
         })
         .catch(function() {
-          return caches.match("./index.html?v=68");
+          return caches.match("./index.html?v=69");
         })
     );
     return;
